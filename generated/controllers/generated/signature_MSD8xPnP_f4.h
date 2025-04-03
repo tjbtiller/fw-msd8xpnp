0b350b247c19f757cd5984c21880142da452d41c
GitHub git update Action <action@github.com>
2025-04-03 00:45:26 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_MSD8xPnP_f4.h
+++ generated/controllers/generated/signature_MSD8xPnP_f4.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2725249858
-#define TS_SIGNATURE "rusEFI main.2025.04.02.MSD8xPnP_f4.2725249858"
+#define SIGNATURE_HASH 324461773
+#define TS_SIGNATURE "rusEFI main.2025.04.03.MSD8xPnP_f4.324461773"
